f64836b9ce7524a3a4da66510958c258c5e58418
Nguyễn Hoàng <hoangboo1997@gmail.com>
2021-01-14 12:17:37 +0700

    change the name to facades db

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
-                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Member <i class="fa fa-fw fa-caret-down"></i></a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Facades DB<i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="demo" class="collapse">
                     <li>
                         <a href="{{ route('admin.list-member') }}">List Member</a>
